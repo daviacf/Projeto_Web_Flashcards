056cf65665070e9e37efc76da51c104dc0642fa9
Victor Raphael <157927292+victorraphaemilk@users.noreply.github.com>
2026-06-28 21:39:49 -0300

    fix: ajusta config com prometheus

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 
+from prometheus_flask_exporter import PrometheusMetrics
+
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = 'uma-chave-secreta-muito-segura-e-diferente'
     CORS(app)
+
+    metrics = PrometheusMetrics(app)
 
     from .controllers.user_controller import usuario_bp
     from .controllers.deck_controller import baralho_bp
